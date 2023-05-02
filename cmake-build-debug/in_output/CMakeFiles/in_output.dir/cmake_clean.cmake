33dc529dd0b45c16ebaa088bcfe80becbdfc852b
dongju <ehdqkdehdwn@gmail.com>
2023-05-02 23:57:22 +0900

    first commit

---
+++ cmake-build-debug/in_output/CMakeFiles/in_output.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/in_output.dir/p2557.cpp.o"
+  "CMakeFiles/in_output.dir/p2557.cpp.o.d"
+  "in_output"
+  "in_output.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/in_output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ in_output/p2557.cpp
@@ -0,0 +1,29 @@
+//
+// Created by dongju-kim on 23. 4. 8.
+//
+
+#include <iostream>
+
+int main()
+{
+    int a=0;
+    int b=0;
+    int c=0;
+    int d =0;
+    if((!c||!d==1)&&b==0)
+    {
+        std::cout<<"hi"<<std::endl; std::cout<<"dkdkd"<<std::endl;
+    }
+    else
+    {
+        std::cout<<"umm\n"<<std::endl;
+    }
+
+    return 0;
+}
+
+/*
+ 2558, 10950, 10951,
+10952, 10953, 11021, 11022, 11718, 11719, 11720, 11721,
+2741, 2742, 2739, 1924, 8393, 10818, 2438,
+2439, 2440, 2441, 2442, 2445, 2522, 2446, 10991, 10992*/
